58fd2a5e49de29657a9fb89eb0ecb4e7d4bb4784
tlfobe <lennyfobe@gmail.com>
2019-10-24 20:58:44 -0600

    adjusted data_viz from last wee

---
+++ data_viz.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+
+
+def boxplot(L, out_file_name="boxplot.png", names = None, x_label = "", y_label = "", title = ""):
+    """
+    constructs a boxplot and writes the plot to the specified filename
+    Arguments
+    ---------
+    L : list of lists
+        list of lists of numbers that will be put into a boxplot
+    """
+    if L is None:
+        raise TypeError("boxplot : Please supply a list and")
+
+    if not isinstance(L, (list, np.ndarray)):
+        raise TypeError("boxplot : Incorrect input type, "
+                        + "please supply a list!")
+
+    if len(L) == 0:
+        raise IndexError("boxplot : Unpopulated list!")
+
+    list_types = [not isinstance(val, list)
+                  for val in L]
+
+    if any(list_types):
+        raise TypeError("boxplot : List contains invalid type!")
+
+    if type(out_file_name) != str:
+        raise TypeError("boxplot : filename is invalid type!")
+
+    else:
+        fig = plt.figure(figsize=(3, 3), dpi=300)
+        ax = fig.add_subplot(1, 1, 1)
+        ax.title.set_text(title)
+        ax.set_ylabel(y_label)
+        ax.set_xlabel(x_label)
+
+        ax.boxplot(L)
+        plt.xticks(list(range(1, len(names)+1)), names)
+
+    try:
+        plt.savefig(out_file_name, bbox_inches='tight')
+    except PermissionError:
+        raise PermissionError(
+            "boxplot : Unable to write to "+out_file_name +
+            ". Check file permissions!")
+    except ValueError:
+        raise ValueError(
+            "boxplot : Incompatible file extension on "+out_file_name)
---
+++ test_data_viz.py
@@ -0,0 +1,53 @@
+import unittest
+import os
+import data_viz
+import stat
+
+
+class TestBoxPlot(unittest.TestCase):
+    def test_boxplot_null(self):
+        self.assertRaises(TypeError,
+                          data_viz.boxplot,
+                          None,
+                          )
+
+    def test_boxplot_empty_list(self):
+        self.assertRaises(IndexError,
+                          data_viz.boxplot,
+                          [],
+                          )
+
+    def test_boxplot_wrong_type(self):
+        self.assertRaises(TypeError, data_viz.boxplot, 'string', 'file.png')
+        self.assertRaises(TypeError, data_viz.boxplot, dict(), 'file.png')
+        self.assertRaises(TypeError, data_viz.boxplot, True, 'file.png')
+
+    def test_boxplot_list_wrong_type(self):
+        self.assertRaises(TypeError, data_viz.boxplot,
+                          ['string', 1, 2, ['List!'], ],
+                          )
+
+    def test_boxplot_filename_str(self):
+        self.assertRaises(TypeError, data_viz.boxplot,
+                          [[1, 2, 3], [4, 5, 6], [7]],
+                          1,
+                          )
+
+    def test_boxplot_write_to_file(self):
+        data_viz.boxplot([[1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]], 'newfile.png',  names=["test"])
+        self.assertTrue(os.path.exists("newfile.png"))
+        os.remove("newfile.png")
+
+    def test_boxplot_permission(self):
+        with open("read_only.png", "w") as f:
+            f.write("Hello, World!")
+        os.chmod("read_only.png", stat.S_IREAD | stat.S_IRGRP | stat.S_IROTH)
+        self.assertRaises(PermissionError, data_viz.boxplot, [
+                          [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]], "read_only.png",
+                          names = ["yes"]
+                          )
+        os.remove("read_only.png")
+
+    def test_boxplot_wrong_file(self):
+        self.assertRaises(ValueError, data_viz.boxplot,
+                          [[1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]], "read_only.txt", names = ["test"])
